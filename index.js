88f18839c1e2fcf09a56c54a293769d31da3fd8a
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-05-25 12:12:58 +0600

    sendingOrders

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config();
@@ -13,12 +13,46 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ot7tc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    client.connect()
+    const partsCollection = client.db("cycle").collection("parts");
+    const orderCollection = client.db("cycle").collection("orders");
+    try {
+        app.get('/parts', async (req, res) => {
+            const query = {}
+            const cursor = await partsCollection.find(query).toArray()
+            res.send(cursor)
+        })
+        app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await partsCollection.findOne(query)
+            res.send(result)
+        })
+        app.put('/parts/:id', async (req, res) => {
+            const id = req.params.id
+            const quantity = req.body
+            console.log(quantity)
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: quantity
+            }
+            const result = await partsCollection.updateOne(filter, updatedDoc, options)
+            res.send(result)
+        })
+        app.post('/orders', async (req, res) => {
+            const query = req.body
+            const result = await orderCollection.insertOne(query)
+            res.send(result)
+        })
+
+    } finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
